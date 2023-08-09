a2fb693f15d22b97f13b45e52d3ecf9856212874
pedropaulofb <pedropaulofb@gmail.com>
2023-08-09 18:10:05 +0200

    project structuring

---
+++ ontouml-graph-validator/main.py
@@ -0,0 +1,11 @@
+""" Main module for validating OntoUML Graphs. """
+def validate():
+
+    # Load model
+    # perform validation
+    # present validation results
+
+    pass
+
+if __name__ == '__main__':
+    validate()
